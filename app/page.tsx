27526f606d3c7e5183957923e0c7e6b67d5c4a16
Joel Kizito <joelkizito@college.harvard.edu>
2025-10-01 17:57:11 -0400

    Update project with updated Aug 2025 dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,13 @@ import { TypographyH2, TypographyP } from "@/components/ui/typography";
 export default function Home() {
   return (
     <>
-      <TypographyH2>Welcome to the T4SG starter project!</TypographyH2>
+      <TypographyH2>Welcome to the T4SG Lamplight Project!</TypographyH2>
       <TypographyP>
         This starter project is styled with Tailwind CSS and uses shadcn/ui as a component library. Feel free to add
         your own components!
       </TypographyP>
       <TypographyP>
-        This page is an unprotected route accessible to anyone who visits the website. Log in to view authenticated
-        routes!
+        This page is an unprotected route accessible to anyone who visits the website. Log in to view more!
       </TypographyP>
     </>
   );
