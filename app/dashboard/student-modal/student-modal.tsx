995661d0e8de11a70a52722a4d57b0e4afde1ea8
Owen Wang <owenwang2007@icloud.com>
2025-10-22 20:44:05 -0400

    feat(student-modal): add dialog functionality

--- app/dashboard/student-modal/student-modal.tsx
+++ app/dashboard/student-modal/student-modal.tsx
@@ -1,10 +1,21 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { createServerSupabaseClient } from "@/lib/server-utils";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+import { useState } from "react";
 import InfoForm from "./info-form";
 
 export default async function StudentModal({ studentId }: { studentId: string }) {
   const supabase = createServerSupabaseClient();
   const { data: studentList, error } = await supabase.from("students").select().eq("id", studentId);
+  const [open, setOpen] = useState<boolean>(false);
 
   if (error) {
     return <div>Error loading student: {error.message}</div>;
@@ -17,22 +28,33 @@ export default async function StudentModal({ studentId }: { studentId: string })
   const student = studentList[0];
 
   return (
-    <>
-      <h1>
-        {student.legal_first_name} {student.legal_last_name} ({student.preferred_name})
-      </h1>
-      <Tabs defaultValue="info">
-        <TabsList>
-          <TabsTrigger value="info">Basic Info</TabsTrigger>
-          <TabsTrigger value="learning-profile">Learning Profile</TabsTrigger>
-          <TabsTrigger value="advising">Advising</TabsTrigger>
-        </TabsList>
-        <TabsContent value="info">
-          <InfoForm student={student}></InfoForm>
-        </TabsContent>
-        <TabsContent value="learning-profile">Learning Profile</TabsContent>
-        <TabsContent value="advising">Advising</TabsContent>
-      </Tabs>
-    </>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button>+</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {student.legal_first_name} {student.legal_last_name}
+          </DialogTitle>
+          <DialogDescription>({student.preferred_name})</DialogDescription>
+        </DialogHeader>
+        <h1>
+          {student.legal_first_name} {student.legal_last_name} ({student.preferred_name})
+        </h1>
+        <Tabs defaultValue="info">
+          <TabsList>
+            <TabsTrigger value="info">Basic Info</TabsTrigger>
+            <TabsTrigger value="learning-profile">Learning Profile</TabsTrigger>
+            <TabsTrigger value="advising">Advising</TabsTrigger>
+          </TabsList>
+          <TabsContent value="info">
+            <InfoForm student={student}></InfoForm>
+          </TabsContent>
+          <TabsContent value="learning-profile">Learning Profile</TabsContent>
+          <TabsContent value="advising">Advising</TabsContent>
+        </Tabs>
+      </DialogContent>
+    </Dialog>
   );
 }
